885e4bfbe852a43bd7c6168cd6d957942031aebb
Vivek's Applications <vivek.kumar450028@gmail.com>
2017-01-22 13:32:04 +0000

    Production Ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    
+before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 def index
 
@@ -28,17 +30,17 @@ end
 
 def show
 
-@article = Article.find(params[:id])
+
 
 end
 def edit
 
-@article = Article.find(params[:id])
+
 
 end
 def update
 
-@article = Article.find(params[:id])
+
 
 if @article.update(article_params)
 
@@ -55,7 +57,7 @@ end
 end
 def destroy
 
-@article = Article.find(params[:id])
+
 
 @article.destroy
 
@@ -65,6 +67,11 @@ redirect_to articles_path
 
 end
 private
+def set_article
+
+@article = Article.find(params[:id])
+
+end
 def article_params
 
 params.require(:article).permit(:title, :description)
