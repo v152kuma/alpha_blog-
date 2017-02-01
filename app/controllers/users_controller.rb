d3b2a9f70f065084c2c77963323fb539bb6dbe12
Vivek's Applications <vivek.kumar450028@gmail.com>
2017-02-01 11:20:24 +0000

    Sign in upon Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,11 @@ def create
 
 if @user.save
 
+session[:user_id] = @user.id
+
 flash[:success] = "Welcome to the alpha blog #{@user.username}"
 
-redirect_to articles_path
+redirect_to user_path(@user)
 
 else
 
@@ -33,6 +35,7 @@ render 'new'
 end
 
 end
+
 
 def edit
 
