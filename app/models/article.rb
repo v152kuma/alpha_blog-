977dbc288fd43a46ed8811afb43909a43e6fbda9
Vivek's Applications <vivek.kumar450028@gmail.com>
2017-02-17 14:31:01 +0000

    Edit categories added

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ActiveRecord::Base
     belongs_to :user
     validates :title ,presence:true ,length: {minimum: 3,maximum: 50}
     validates :description ,presence:true ,length: {minimum: 10,maximum: 5000}
-    
+    has_many :article_categories
+    has_many :categories, through: :article_categories
+
 end
